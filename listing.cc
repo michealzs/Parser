93b00d30f2a0c4eed940f8039bcaa929f68e4a4c
michealzs <seuntuyie@gmail.com>
2024-01-15 15:24:51 -0500

    update 1.15

--- listing.cc
+++ listing.cc
@@ -1,77 +1,98 @@
-
-
 #include <cstdio>
 #include <string>
-#include <queue>
-#include <array>
+#include <queue> // Include queue for storing error messages
+
 using namespace std;
 
 #include "listing.h"
 
-static int lineNumber;
+static int lineNumber; // Static variable for keeping track of line numbers
+static int totalErrors = 0; // Static variable to count the total number of errors
 
-//static string error = ""; 
+static int lexicalErrors = 0;
+static int syntaxErrors = 0;
+static int generalSemanticErrors = 0;
+static int duplicateIdentifierErrors = 0;
+static int undeclaredIdentifierErrors = 0;
+static queue<string> errorQueue; // Queue to store error messages
 
-static int totalErrors = 0;
+void displayErrors();
 
-static array<int, 3> errorCounts = {0, 0, 0}; // {lexical, syntactic, semantic}
-static queue<string> errorMessages;
-
-
-//static void displayErrors(); 
-
+// Function to initialize and print the first line number
 void firstLine()
 {
-	lineNumber = 1;
-	printf("\n%4d  ",lineNumber);
+    lineNumber = 1; // Initialize line number to 1
+    printf("\n%4d  ", lineNumber); // Print the line number with formatting
 }
 
+// Function to print the next line number and handle errors
 void nextLine()
 {
-	displayErrors();
-	lineNumber++;
-	printf("%4d  ",lineNumber);
+    displayErrors(); // Display any errors that occurred on the current line
+    lineNumber++; // Increment the line number
+    printf("%4d  ", lineNumber); // Print the line number with formatting
 }
 
+// Function to handle the last line of the listing
 int lastLine()
 {
-	printf("\r");
-	displayErrors();
-	printf("     \n");
+    printf("\r");
+    displayErrors(); // Display any errors for the last line
 
-	if (totalErrors > 0) {
-        printf("Total Errors: %d\n", totalErrors);
-        printf("Lexical Errors: %d, Syntactic Errors: %d, Semantic Errors: %d\n",
-               errorCounts[0], errorCounts[1], errorCounts[2]);
-    } else {
+    if (totalErrors == 0)
+    {
         printf("Compiled Successfully\n");
     }
+    else
+    {
+        printf("Lexical Errors %d\n", lexicalErrors);
+        printf("Syntax Errors %d\n", syntaxErrors);
+        printf("General Semantic Errors %d\n", generalSemanticErrors);
+        printf("Duplicate Identifier Errors %d\n", duplicateIdentifierErrors);
+        printf("Undeclared Identifier Errors %d\n", undeclaredIdentifierErrors);
+    }
 
-	return totalErrors;
+    return lineNumber;
 }
-    
+
+// Function to append an error message to the current error
 void appendError(ErrorCategories errorCategory, string message)
 {
-	string messages[] = { "Lexical Error, Invalid Character ", "",
-		"Semantic Error, ", "Semantic Error, Duplicate ",
-		"Semantic Error, Undeclared " };
+    string messages[] = { "Lexical Error, Invalid Character ", "",
+        "Semantic Error, ", "Semantic Error, Duplicate ",
+        "Semantic Error, Undeclared " };
 
-	errorMessages.push(messages[errorCategory] + message);
-	totalErrors++;
-	errorCounts[errorCategory]++;
-	//error = messages[errorCategory] + message;
-	
+    // Count errors based on category
+    switch (errorCategory)
+    {
+        case LEXICAL:
+            lexicalErrors++;
+            break;
+        case SYNTAX:
+            syntaxErrors++;
+            break;
+        case GENERAL_SEMANTIC:
+            generalSemanticErrors++;
+            break;
+        case DUPLICATE_IDENTIFIER:
+            duplicateIdentifierErrors++;
+            break;
+        case UNDECLARED:
+            undeclaredIdentifierErrors++;
+            break;
+    }
+
+    // Concatenate error category message with the custom message
+    errorQueue.push(messages[errorCategory] + message);
+    totalErrors++; // Increment total error count
 }
 
+// Function to display all the error messages that have occurred on the previous line
 void displayErrors()
 {
-	while (!errorMessages.empty()) {
-    	printf("%s\n", errorMessages.front().c_str());
-        errorMessages.pop();
+    while (!errorQueue.empty())
+    {
+        printf("%s\n", errorQueue.front().c_str());
+        errorQueue.pop();
     }
-	/*
-	if (error != "")
-		printf("%s\n", error.c_str());
-	error = "";
-	*/
 }
